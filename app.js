20cc3b0c5965aa5a821ea103a1d9bd7377c9c7ae
chaitali12k <chaitalikothule2015.com>
2023-10-07 11:42:33 +0530

    blog project correction

--- app.js
+++ app.js
@@ -11,7 +11,8 @@ app.use("/api/user",router);
 app.use("/api/blog",blogRouter);
 
 app.get("/",(req,res) => {
-    res.status.send("<h1>Welcome to Blog World</h1>")
+    res.status(200).send("Response Data");
+
 });
 
 mongoose.connect('mongodb+srv://admin:UTLVeZy4uQYVPq3C@cluster0.ohnzrzl.mongodb.net/?retryWrites=true&w=majority').then(()=>app.listen(5000)).then(()=>console.log("connected to database")).catch((err)=>console.log(err));
